d7d6756c9704c05231be906ffa22af2f0f1f0fe9
Jasper-22 <jjasper22@gmail.com>
2021-03-18 09:46:53 +0200

    Building global-settings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
@@ -41,6 +42,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
 
     ClarityModule,
 
--- src/app/global-settings-page/global-settings-page.component.ts
+++ src/app/global-settings-page/global-settings-page.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { AppConfig } from '../../environments/environment';
+
+import { grpc } from "@improbable-eng/grpc-web";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import { VSTestServer } from '../proto/VTestService_pb_service';
+import { ServerHealthStatus } from '../proto/VTestService_pb';
 
 @Component({
   selector: 'app-global-settings-page',
@@ -7,9 +15,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalSettingsPageComponent implements OnInit {
 
-  constructor() { }
+  targetServerForm = new FormGroup({
+    serverAddress: new FormControl(AppConfig.GRPCWebServerAddress)
+  });
+
+  serverErrorOccurred: boolean;
+  serverConnectionInProgress: boolean;
+
+  constructor() {
+
+    this.serverErrorOccurred = false;
+    this.serverConnectionInProgress = false;
+
+    this.targetServerForm.controls['serverAddress'].valueChanges.subscribe({
+      next: (x) => this.serverErrorOccurred = false,
+      error: (x) => this.serverErrorOccurred = true,
+      complete: () => {}
+    });
+
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.serverConnectionInProgress = true;
+
+    grpc.invoke(VSTestServer.HealthStatus, {
+        host: this.targetServerForm.controls['serverAddress'].value,
+        request: new Empty(),
+        onMessage: (serverHealthStatus: ServerHealthStatus) => {
+          this.serverConnectionInProgress = false;
+
+          this.showServerHealthStatus(serverHealthStatus)
+        },
+        onEnd: (code: grpc.Code, msg: string | undefined, trailers: grpc.Metadata) => {
+          this.serverConnectionInProgress = false;
+
+          if (code == grpc.Code.OK) {
+              // All ok
+          } else {
+            this.healthStatusError(code, msg, trailers);
+          }
+        }
+    });
+  }
+
+  healthStatusError(code: grpc.Code, msg: string | undefined, trailers: grpc.Metadata) {
+    this.serverErrorOccurred = true;
+    this.targetServerForm.controls.serverAddress.setErrors({ invalidGrpcServer:true });
+  }
+
+  showServerHealthStatus(serverHealthStatus: ServerHealthStatus) {
+
+  }
 }
