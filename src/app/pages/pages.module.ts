a1fc937e0e52b333d6f4cb8f42e8b2387ac5f9ae
Jasper-22 <jjasper22@gmail.com>
2021-04-04 16:06:54 +0300

    Cleaned all unused files

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,18 +6,14 @@ import { ClarityModule } from '@clr/angular';
 
 import { ComponentsModule } from '../components';
 
-// import { DetailComponent } from './detail/detail.component';
 import { GlobalSettingsPageComponent} from './global-settings-page/global-settings-page.component';
-import { HomeComponent } from './home/home.component';
 import { ReportsPageComponent } from './reports-page/reports-page.component';
 import { RunningTestsPageComponent } from './running-tests-page/running-tests-page.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-   // DetailComponent,
     GlobalSettingsPageComponent,
-    HomeComponent,
     ReportsPageComponent,
     RunningTestsPageComponent,
     WelcomePageComponent
